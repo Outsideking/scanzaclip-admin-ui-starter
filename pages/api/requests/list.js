a2f9d597e3f09b66260e5bf3b49ff12c218e8d7c
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 10:07:45 +0700

    Delete pages/api/requests directory

--- pages/api/requests/list.js
+++
@@ -1,8 +0,0 @@
-// pages/api/admin/requests/list.js
-import { db } from '../../../../lib/firebaseAdmin.js';
-
-export default async function handler(req, res){
-  const snap = await db.collection('permission_requests').orderBy('createdAt', 'desc').limit(200).get();
-  const items = snap.docs.map(d => ({ id: d.id, ...d.data() }));
-  res.status(200).json({ items });
-}
